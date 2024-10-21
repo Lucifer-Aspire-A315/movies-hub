d3a1c91c95d7d3651edcaf6c03c911c62b407bd7
lucifer <131165553+Lucifer-Aspire-A315@users.noreply.github.com>
2024-10-21 06:44:01 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Pages/HomePage/Home.jsx';
-import MovieDetail from './Pages/MovieDetail/MovieDetail.jsx';
-import SearchBar from './Components/SearchBox/SearchBar.jsx';
-import TopRated from './Pages/Top-Rated/TopRated.jsx';
-import UpComing from './Pages/Up-Coming/UpComing.jsx';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/HomePage/Home.jsx";
+import MovieDetail from "./Pages/MovieDetail/MovieDetail.jsx";
+import TopRated from "./Pages/Top-Rated/TopRated.jsx";
+import UpComing from "./Pages/Up-Coming/UpComing.jsx";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Top-Rated/" element={<TopRated/>}/>
+        <Route path="/Top-Rated/" element={<TopRated />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
-        <Route path="/Up-Coming/" element={<UpComing/>} />
+        <Route path="/Up-Coming/" element={<UpComing />} />
       </Routes>
     </Router>
   );
